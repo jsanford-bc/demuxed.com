3f295dd7698533a24d675c277611af78a11dc464
Matthew McClure <matt@mmcc.io>
2015-08-29 15:32:46 -0400

    add notice programmatically

--- _harp/public/js/app.js
+++ _harp/public/js/app.js
@@ -1,7 +1,33 @@
+function parseQueryParams(params) {
+  if (!params) return {};
+
+  // Remove the ?
+  params = params.substring(1);
+
+  var splitParams = params.split('&');
+
+  // Turn the params into an object
+  var paramsObj = {};
+  splitParams.forEach(function(val, i) {
+    var splitVal = val.split('=');
+    var key = splitVal[0];
+    var value = splitVal[1];
+
+    paramsObj[key] = value;
+  });
+
+  return paramsObj;
+}
+
 (function(w, d) {
+  var queryParams = parseQueryParams(window.location.search);
+
   // We want to show the flash notification on the vidtechcon.com domain
-  if (w.location.host === 'vidtechcon.com') {
-    var flashNotice = d.getElementById('name-change-alert');
-    flashNotice.classList.remove("hidden");
+  if (w.location.host === 'vidtechcon.com' || queryParams.vtc === '1') {
+    var headerEl = d.getElementsByTagName('header')[0];
+    var flashNotice = d.createElement('div');
+    flashNotice.classList.add('flash-notice');
+    flashNotice.innerText = "You're in the right place, VidTechCon is now Demuxed!";
+    d.body.insertBefore(flashNotice, headerEl);
   }
 })(window, document);
