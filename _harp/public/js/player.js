3cc6c7f1c9bcf0ddaaf3b6ca6839ff193730862d
Justin Sanford <justin.h.sanford@gmail.com>
2015-10-01 09:03:14 -0700

    updated urls

--- _harp/public/js/player.js
+++ _harp/public/js/player.js
@@ -1,24 +1,9 @@
 videojs('livePlayer').ready(function() {
   var player = this;
   console.log('this');
-  
-  if (player.dvrux) {
-    player.dvrux({
-      "bookendBefore": {
-        "startTime": 1443714300000,
-        "type": "text",
-        "src": "This live event has not yet started"
-      },
-      "bookendAfter": {
-        "startTime": 1443751200000,
-        "type": "text",
-        "src": "Thank you for watching!"
-      }
-    });
-  }
 
   // load the video
-  player.catalog.getVideo("4521229477001", function(err, video) {
+  player.catalog.getVideo("4522547367001", function(err, video) {
     if (err) {
       // do something here
     }
